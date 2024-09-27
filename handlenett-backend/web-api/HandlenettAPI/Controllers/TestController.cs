cecfc34e977a4111ce99f7cb651bf01cecd064c0
Roger Torkelsen <roger.torkelsen@miles.no>
2024-09-27 13:26:44 +0200

    test

--- handlenett-backend/web-api/HandlenettAPI/Controllers/TestController.cs
+++ handlenett-backend/web-api/HandlenettAPI/Controllers/TestController.cs
@@ -16,7 +16,7 @@ namespace HandlenettAPI.Controllers
         [HttpGet]
         public ActionResult<string> Get()
         {
-            return Ok("Hello from Azure v9");
+            return Ok("Hello from Azure v10");
         }
     }
 }

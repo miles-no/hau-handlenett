f3c66ff014a4ca53b17266b2da0dec4d7e5ecae2
Mads Tordal <mads.tordal@miles.no>
2024-10-03 09:11:15 +0200

    handlenett-frontend: make item deletable
    .. for a users 'own' items.
    
    This became a feature because the partitionkey in db is username.
    More easy to delete own elements than others when sending just an
    id. By sending an id and wanting to delete others we need to do a lookup
    first. Could you say it is a bug? You might. But this is a feature :D

--- handlenett-frontend/components/Item.vue
+++ handlenett-frontend/components/Item.vue
@@ -1,26 +1,33 @@
 <template>
     <div class="groceryitem">
-        <label :class="{ 'complete': props.isComplete }">
-            <input type="checkbox" :checked="props.isComplete" @change="update"> {{ props.name }}
+        <label :class="{ 'complete': status }">
+            <input type="checkbox" :checked="status" @change="update"> {{ props.element?.name }}
         </label>
-        <button @click="deleteItem">X</button>
+        <button v-if="isElementDeletable" @click="deleteeleement">X
+        </button>
     </div>
 </template>
 <script setup>
-
+import { ref } from 'vue';
 const emit = defineEmits(['changed'])
 
+const status = ref(false)
+
 const props = defineProps({
-    id: { type: String },
-    name: { type: String },
-    isComplete: { type: Boolean, default: false }
+    element: { type: Object },
+    isElementDeletable: { type: Boolean, default: false },
+})
+
+onMounted(() => {
+    status.value = props.element.isCompleted
 })
 
 const update = () => {
-    emit('changed', { name: props.name, isComplete: !props.isComplete, id: props.id })
+    status.value = !status.value;
+    emit('changed', { id: props.element.id, name: props.element.name, isCompleted: status.value })
 }
-const deleteItem = () => {
-    emit('delete', { id: props.id })
+const deleteeleement = () => {
+    emit('delete', { id: props.element.id })
 }
 
 </script>
--- handlenett-frontend/pages/index.vue
+++ handlenett-frontend/pages/index.vue
@@ -2,22 +2,25 @@
     <div v-if="loading" class="loadData">
         <div class="loader"></div>
     </div>
-
     <h1>Handlenett</h1>
     <div>
         <div style="margin-bottom: 2rem;">
-            <Item v-for="i in items" :name="i.name" :isComplete="i.isComplete" :id="i.id" :key="i.id"
-                @changed="updatedItem" @delete="deleteItem" />
+            <Item v-for="i in items" :key="i.id" @changed="updatedItem" @delete="deleteItem"
+                :isElementDeletable="currentUser === i.createdBy" :element="i" />
         </div>
         <NewItem @changed="newItem"></NewItem>
     </div>
-
+    <!-- <pre>
+        {{ items }}
+     </pre> -->
 </template>
 <script setup>
 import { ref } from 'vue'
-const items = ref([])
+const { $getAccounts } = useNuxtApp();
 
+const items = ref([])
 const loading = ref(true)
+const currentUser = ref("")
 
 onMounted(async () => {
 
@@ -26,13 +29,18 @@ onMounted(async () => {
         items.value = data;
     });
     loading.value = false
+
+    currentUser.value = await $getAccounts()[0].username;
+
 })
 
 const updatedItem = (updatedItem) => {
-    useHttp(`Item/${updatedItem.id}`, 'PUT', { name: updatedItem.name, isComplete: updatedItem.isComplete }).then(data => {
+    console.info('updatedItem', updatedItem)
+    useHttp(`Item/${updatedItem.id}`, 'PUT', { name: updatedItem.name, isCompleted: updatedItem.isCompleted }).then(data => {
         let i = items.value.find(i => i.id === updatedItem.id)
         const idx = items.value.indexOf(i)
-        items.value[idx] = updatedItem;
+        console.log('updated', data)
+        items.value[idx] = data;
     });
 }
 
@@ -40,7 +48,7 @@ const newItem = (newItem) => {
     if (newItem.name === '') return
 
     useHttp('Item', 'POST', { name: newItem.name }).then(data => {
-        items.value.push({ name: data.name, isComplete: data.isComplete, id: data.id })
+        items.value.push(data)
     });
 
 }
